ef136c69ebbf73b96dcb9518117c366eb6259116
Aleksandr2905 <natalukha@ukr.net>
2023-11-20 13:07:19 +0200

    update style home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import image from '../components/image/smartphone.jpg';
 import css from './Page.module.css';
 
 export const HomePage = () => {
   return (
-    <div>
+    <>
       <h2 className={css.homeTitle}>Phonebook</h2>
-      <img className={css.homeImg} src={image} alt="phone" />
-    </div>
+    </>
   );
 };
